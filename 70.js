c63bd646e802aa7010157e80761f40d28e5b0113
hesam <hesamshayegan88@gmail.com>
2023-11-29 15:40:20 -0800

    added 70. Climbing Stairs

---
+++ 70.js
@@ -0,0 +1,25 @@
+// 70. Climbing Stairs
+
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+
+    let prev = 0;
+    let curr = 1;
+    let temp;
+
+    for (let i = 0; i < n; i++) {
+        temp = prev;
+        prev = curr;
+        curr += temp;
+    }
+
+    return curr;
+
+};
+
+// stairs = 1, 2, 3, 4, 5,...
+// ways = 1, 2, 3, 5, 8
