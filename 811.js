a05e4809f80f29eca5c8d0db88d408d60c2d52b8
hesam <hesamshayegan88@gmail.com>
2024-02-17 16:15:24 -0800

    modified the solution for 811. Subdomain Visit Count

--- 811.js
+++ 811.js
@@ -4,7 +4,41 @@
 /**
  * @param {string[]} cpdomains
  * @return {string[]}
- */
+*/
+
+
+const subdomainVisits = function(cpdomains) {
+    const visitsMap = {};
+
+    for (const domain of cpdomains) {
+        const [count, fullDomain] = domain.split(' ');
+        const subdomains = extractSubdomains(fullDomain);
+
+        for (const sub of subdomains) {
+            visitsMap[sub] = (visitsMap[sub] || 0) + Number(count);
+        }
+    }
+
+    const result = [];
+    for (const subdomain in visitsMap) {
+        result.push(`${visitsMap[subdomain]} ${subdomain}`);
+    }
+
+    return result;
+};
+
+function extractSubdomains(domain) {
+    const parts = domain.split(".");
+    const subdomains = [];
+
+    for (let i = 0; i < parts.length; i++) {
+        subdomains.push(parts.slice(i).join("."));
+    }
+
+    return subdomains;
+}
+
+/*
 var subdomainVisits = function(cpdomains) {
 
     let freq = {};
@@ -34,3 +68,5 @@ var subdomainVisits = function(cpdomains) {
     return res
 
 }
+
+*/
