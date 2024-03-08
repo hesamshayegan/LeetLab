efc4dfa0cffa76ac34ddb7dea15c9f64fac6d866
hesam <hesamshayegan88@gmail.com>
2024-03-08 12:08:33 -0800

    added 334. Increasing Triplet Subsequence + 1657. Determine if Two Strings Are Close

---
+++ 334.js
@@ -0,0 +1,29 @@
+// 334. Increasing Triplet Subsequence
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+*/
+
+var increasingTriplet = function(nums) {
+
+    let firstNumber = Infinity;
+    let secondNumber = Infinity;
+
+    for (let currentNumber of nums) {
+        
+        if(currentNumber > secondNumber && currentNumber > firstNumber) {
+            return true
+        }
+
+        // update firstNumber & secondNumber
+        if(currentNumber > firstNumber) {
+            secondNumber = currentNumber
+        } else {
+            firstNumber = currentNumber
+        }
+    };
+
+    return false
+    
+};
